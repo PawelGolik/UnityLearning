382d7f6c2ffa5eb39069b63da32812787bfbd713
PawelGolik <209319@student.pwr.edu.pl>
2018-09-15 15:23:21 +0200

    Adding obsticles and restart level after hiting obsticle plus fuel componnents

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,30 +1,59 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 using System;
 
 public class Rocket : MonoBehaviour {
 
+    [SerializeField]
+    float RCSSpeed = 100f;
+    [SerializeField]
+    float mainThrust = 100f;
     private Rigidbody rigidBody;
     private AudioSource audioSource;
     private GameObject Camera;
+    private Vector3 startingPosition;
+    private Quaternion startingRotation;
     // Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
         Camera = GameObject.Find("Main Camera");
         audioSource = GetComponent<AudioSource>();
+        startingPosition = transform.position;
+        startingRotation = transform.rotation;
         audioSource.playOnAwake = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
-        
-	}
-    private void ProcessInput()
+        Thrust();
+        Rotation();
+        HoldCameraOnRocket();
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Noting happent");
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            case "Finish":
+                print("Finish");
+                break;
+            default:
+                RestartLevel();
+                break;
+        }
+    }
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -37,15 +66,27 @@ public class Rocket : MonoBehaviour {
                 audioSource.Pause();
             }
         }
+    }
+    private void Rotation()
+    {
+        rigidBody.freezeRotation = true;
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(-Vector3.forward * Time.deltaTime * 100, Space.Self);
+            transform.Rotate(-Vector3.forward * Time.deltaTime * RCSSpeed, Space.Self);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(Vector3.forward * Time.deltaTime * 100, Space.Self);
+            transform.Rotate(Vector3.forward * Time.deltaTime * RCSSpeed, Space.Self);
         }
-        Camera.transform.position = new Vector3(rigidBody.transform.position.x, rigidBody.transform.position.y , rigidBody.transform.position.z -9);
+        rigidBody.freezeRotation = false;
     }
+    private void HoldCameraOnRocket()
+    {
+        Camera.transform.position = new Vector3(rigidBody.transform.position.x, rigidBody.transform.position.y + 5, rigidBody.transform.position.z  + 25);
 
+    }
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
